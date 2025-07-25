9bc18cdb55db13ed87fd7c0369489a2feb88764e
onPV <phivalle@gmail.com>
2025-07-25 15:28:18 +0200

    Phase 1 - render deploy - fix env var

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,21 +1,8 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-// Ne charge le fichier .env que si on n'est PAS en mode production
-if (!isset($_SERVER['APP_ENV']) || $_SERVER['APP_ENV'] !== 'prod') {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
-
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
-    Debug::enable();
-}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
