06cfd78ccffefaa3be7df39904d099eb06dd1431
onPV <phivalle@gmail.com>
2025-07-25 18:45:52 +0200

    Phase 1 - render deploy - fix 2 entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-# Applique les migrations de la base de données
-php bin/console doctrine:migrations:migrate --env=prod--no-interaction
+# La ligne importante est celle-ci, avec --env=prod
+php bin/console doctrine:migrations:migrate --env=prod --no-interaction
 
 # Démarre PHP-FPM en arrière-plan
 php-fpm -F &
