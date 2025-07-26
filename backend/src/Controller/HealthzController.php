675c3119fe1975d8dd0da437e3dc9fd9f6aa0a15
onPV <phivalle@gmail.com>
2025-07-26 19:55:30 +0200

    Pahse 1 - Render deploy - backend et bdd ok.

--- backend/src/Controller/HealthzController.php
+++ backend/src/Controller/HealthzController.php
@@ -1,5 +1,12 @@
 <?php
 
+# test de santé de l'application
+# https://prismeia-backend-staging.onrender.com/healthz
+#
+#   qui répond 200 OK si : si Le Kernel Symfony boot correctement & l’application est fonctionnelle
+#   sinon 500 Internal Server Error
+#endregion
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
@@ -13,3 +20,4 @@ class HealthzController
         return new Response('OK', Response::HTTP_OK, ['Content-Type' => 'text/plain']);
     }
 }
+
