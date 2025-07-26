3eb23526eb04b310c28ac297b3e1916e90d6d79e
onPV <phivalle@gmail.com>
2025-07-26 11:43:31 +0200

    Phase 1 - render deploy - fix 7 : Forçage de l'environnement de production.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "================================================="
-echo "DEBUT DU SCRIPT DE VERIFICATION DE FICHIER"
-echo "Contenu du fichier /var/www/html/bin/console :"
-echo "-------------------------------------------------"
-
-# Affiche le contenu du fichier console dans les logs
-cat /var/www/html/bin/console
-
-echo "-------------------------------------------------"
-echo "FIN DU CONTENU DU FICHIER. Tentative d'execution..."
-echo "================================================="
-
-# On essaie quand même de lancer la commande pour voir l'erreur
+# Cette commande va maintenant fonctionner grâce à la modification dans bin/console
 php bin/console doctrine:migrations:migrate --no-interaction
+
+# Démarre PHP-FPM en arrière-plan
+php-fpm -F &
+
+# Démarre Nginx au premier plan
+nginx -g 'daemon off;'
