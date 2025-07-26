60fd48a319a9310085150ac6315e3e693391f2d3
onPV <phivalle@gmail.com>
2025-07-26 10:33:06 +0200

    Phase 1 - render deploy - fix 3 trace debug entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 set -e
 
-# On peut maintenant retirer le flag --env=prod, car c'est géré dans bin/console
-php bin/console doctrine:migrations:migrate --no-interaction
+echo "================================================="
+echo "DEBUT DU SCRIPT ENTRYPOINT.SH - VERSION DE TEST"
+echo "Si vous voyez ce message, le bon script est execute."
+echo "La commande de migration qui va etre lancee est :"
+echo "php bin/console doctrine:migrations:migrate --env=prod --no-interaction"
+echo "================================================="
+
+# On exécute la VRAIE commande maintenant
+php bin/console doctrine:migrations:migrate --env=prod --no-interaction
+
+echo "================================================="
+echo "MIGRATION TERMINEE. Demarrage des services..."
+echo "================================================="
 
 # Démarre PHP-FPM en arrière-plan
 php-fpm -F &
