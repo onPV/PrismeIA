49e0c30d7958c888657b636fb7e629cd65f4a5a7
onPV <phivalle@gmail.com>
2025-07-26 14:11:02 +0200

    Phase 1 - Render deploy - fix 9fix: avoid .env loading in prod bootstrap.

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -2,11 +2,6 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
-
-// Charge le .env uniquement si APP_ENV n'est pas 'prod'
-if (!isset($_SERVER['APP_ENV']) || $_SERVER['APP_ENV'] !== 'prod') {
-    if (class_exists(Dotenv::class) && file_exists(dirname(__DIR__).'/.env')) {
-        (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-    }
+if (!isset($_SERVER['APP_ENV'])) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
