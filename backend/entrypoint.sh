d2e6236feab84ac5b13c2f6e4fe1632d2f69760f
onPV <phivalle@gmail.com>
2025-07-26 19:28:11 +0200

    Phase 1 - Render deploy - fix 14 : feat  : nginx.conf with prod-ready Symfony/React/healthz routing ,  support frontend SPA + health check in nginx.conf.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,18 +3,13 @@ set -e
 
 echo "🏗️ Initialisation de l'environnement Symfony..."
 
-# S'assurer que les dossiers nécessaires existent
+# Création et permission des répertoires
 mkdir -p var/cache var/log
-
-# Appliquer les permissions correctes
 echo "🔐 Correction des droits sur var/"
 chown -R www-data:www-data var
 chmod -R 775 var
 
-# (Optionnel) Clear cache si nécessaire
-# echo "♻️ Suppression du cache précédent..."
-# php bin/console cache:clear --env=prod || true
-
-# Lancer le process PHP-FPM
-echo "🚀 Lancement de PHP-FPM"
-exec php-fpm
+# Lancement des services supervisés
+echo "🚀 Lancement de PHP-FPM + Nginx"
+php-fpm -D
+nginx -g "daemon off;"
